06a5ea398e693ffec9bb6a7d04cdca902f139f13
apuuuurv <apurvmadaye52@gmail.com>
2026-02-28 21:49:39 +0530

    Edited App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,17 @@
 import { useState } from "react";
+
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import { AnimatePresence } from "framer-motion";
+
 import Layout from "@/components/layout/Layout";
+
 import Dashboard from "./pages/Dashboard";
+
 import Prediction from "./pages/Prediction";
+
 import ClinicalResources from "./pages/ClinicalResources";
+
 import Preloader from "./components/Preloader"; // Make sure to create this file!
 
 function App() {
@@ -13,6 +20,7 @@ function App() {
   return (
     <>
       {/* 1. The Preloader handles its own exit animation */}
+
       <AnimatePresence mode="wait">
         {isLoading && (
           <Preloader key="loader" onComplete={() => setIsLoading(false)} />
@@ -20,12 +28,15 @@ function App() {
       </AnimatePresence>
 
       {/* 2. The main app only renders once loading is done */}
+
       {!isLoading && (
         <BrowserRouter>
           <Layout>
             <Routes>
               <Route path="/" element={<Dashboard />} />
+
               <Route path="/prediction" element={<Prediction />} />
+
               <Route path="/resources" element={<ClinicalResources />} />
             </Routes>
           </Layout>
